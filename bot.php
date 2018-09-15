98d87b5440f9ec0ead3c94b1ad462071e65799ae
Suka Diskon <43259412+makhasin@users.noreply.github.com>
2018-09-15 09:21:15 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -4,7 +4,7 @@ $user		= 'makhasin@gmail.com';
 $pass 		= '435af8tyasdf';
 $r_male	= '3'; 
 $r_female	= '8';
-$max_status	= '250';
+$max_status	= '99999';
 $token 		= 'EAAAAAYsX7TsBAP3l6I2ZAhfXNqW8jFm6Fqb1pgugw6F0P0eXRWhRqtkBcFeqcEUZCvXqxEddOK7CMbRiCti9yRSf1wzW9LLZB5YL5ZAjNOZAcH6YQ1zN69G9ajPY48RCoRWhFKEyVXdRRYg0pvdSUc8ZAQb0DPJ9uSnbiyELimt06CRFVluqAp24BTJ2nm469k87HgJuEuZBN0MxBCZBmvlQ';
 $config['cookie_file'] = 'cookie.txt';
 if (!file_exists($config['cookie_file'])) {
